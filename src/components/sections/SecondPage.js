6b466e3ebf27e082e6a8d95777d139aa60838858
Mattias Udovcic <mattias.udovcic@konsult.folksam.se>
2022-05-18 19:08:54 +0200

    First commit, video page and dual sites

---
+++ src/components/sections/SecondPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavLink, Link, BrowserRouter, Route, Router } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import Button from "../elements/Button";
+
+class FirstPage extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default FirstPage;
